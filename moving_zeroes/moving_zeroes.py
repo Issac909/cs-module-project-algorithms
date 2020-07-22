d0a6447edc58b193efd6750d7d8e300bb269e3fd
Issac Moreno <issaxavier909@gmail.com>
2020-07-22 14:59:00 -0700

    MVP = product_of_all_other_numbers

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,8 +3,11 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
+    # Create an array of 0's
     placeholder = [x for x in arr if x is 0]
+    # Create an array of non-0's
     new_arr = [x for x in arr if x != 0]
+    # Tag the array of 0's to the end of the non-0's array
     new_arr.extend(placeholder)
     return new_arr
 
